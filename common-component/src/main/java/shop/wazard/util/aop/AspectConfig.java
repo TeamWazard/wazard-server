e2f38994f19630a8ff3409a520e9c0c5df7848f5
simhani1 <simhani1@gmail.com>
2023-04-27 02:52:04 +0900

    fix: 에러 메시지 수정

--- common-component/src/main/java/shop/wazard/util/aop/AspectConfig.java
+++ common-component/src/main/java/shop/wazard/util/aop/AspectConfig.java
@@ -19,14 +19,15 @@ class AspectConfig {
 
     private final JwtProvider jwtProvider;
 
+    // 해당 어노테이션을 사용하기 위해서는 메서드의 첫번째 인자가 반드시 accountId로 주어져야 함, 자료형만 동일하면 인식하므로 주의할 것
     @Before("@annotation(shop.wazard.util.aop.Certification) && args(accountId,..)")
     public void certificateWithTokenAndAccountId(Long accountId) throws IllegalStateException {
-        log.info("============== accountID 회원 본인인증 요청 ==============");
+        log.info("============== accountID{}번 회원 본인인증 요청 ==============", accountId);
         ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
         HttpServletRequest request = servletRequestAttributes.getRequest();
         if (!accountId.equals(jwtProvider.getAccountId(request))) {
             log.info("============== 본인인증 실패 ==============");
-            throw new IllegalArgumentException("본인인증 실패");
+            throw new IllegalArgumentException("본인인증에 실패하였습니다.");
         }
     }
 
