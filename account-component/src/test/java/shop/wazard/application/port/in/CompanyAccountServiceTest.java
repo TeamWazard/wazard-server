e7ffc59c983cae704da8a0b58f1641173ca89ece
min9yu98 <kimmingyu9811@gmail.com>
2023-04-26 14:49:52 +0900

    fix: 고용주 회원 정보 수정 단위 테스트 코드 수정

--- account-component/src/test/java/shop/wazard/application/port/in/CompanyAccountServiceTest.java
+++ account-component/src/test/java/shop/wazard/application/port/in/CompanyAccountServiceTest.java
@@ -4,9 +4,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import shop.wazard.adapter.out.persistence.Account;
+import shop.wazard.application.port.out.LoadAccountPort;
 import shop.wazard.application.port.out.SaveAccountPort;
 import shop.wazard.dto.UpdateCompanyAccountInfoReqDto;
 import shop.wazard.dto.UpdateCompanyAccountInfoResDto;
@@ -20,6 +23,8 @@ class CompanyAccountServiceTest {
     private CompanyAccountService companyAccountService;
     @MockBean
     private SaveAccountPort saveAccountPort;
+    @MockBean
+    private LoadAccountPort loadAccountPort;
 
     @Test
     @DisplayName("고용주 - 회원정보 수정 성공")
@@ -35,12 +40,12 @@ class CompanyAccountServiceTest {
                 .message("수정되었습니다.")
                 .build();
 
-        //when
-//        Mockito.doNothing().when(saveAccountPort.updateCompanyAccountInfo(updateCompanyAccountInfoReqDto));
-//
-//        //then
-//        saveAccountPort.updateCompanyAccountInfo(updateCompanyAccountInfoReqDto);
+        // when
+        Account account = loadAccountPort.findByEmail(updateCompanyAccountInfoReqDto.getEmail())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
+        Mockito.doNothing().when(account.updateCompanyAccountInfo(updateCompanyAccountInfoReqDto));
 
+        //then
         Assertions.assertAll(
                 () -> Assertions.assertEquals(updateCompanyAccountInfoResDto.getMessage(), "수정되었습니다.")
         );
