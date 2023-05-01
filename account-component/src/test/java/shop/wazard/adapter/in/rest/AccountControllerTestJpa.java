8dfc6495fb8f62c29f0fd0b3e7cf463826361231
min9yu98 <kimmingyu9811@gmail.com>
2023-05-02 01:19:49 +0900

    test: 비밀번호 확인 controller 테스트 코드 작성

--- account-component/src/test/java/shop/wazard/adapter/in/rest/AccountControllerTestJpa.java
+++ account-component/src/test/java/shop/wazard/adapter/in/rest/AccountControllerTestJpa.java
@@ -1,17 +1,57 @@
 package shop.wazard.adapter.in.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import shop.wazard.WazardApplication;
 
-//@SpringBootTest(classes = WazardApplication.class)
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+
+@SpringBootTest(classes = WazardApplication.class)
 @AutoConfigureMockMvc
 class AccountControllerTestJpa {
 
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @Autowired
-//    private ObjectMapper objectMapper;
-//
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("고용주 - 비밀번호 확인 성공")
+    @WithMockUser
+    public void checkPasswordSuccess() throws Exception {
+        // given
+        String checkPasswordUrl = "/account/check/{accountId}";
+        CheckPasswordReqDto checkPasswordReqDto = CheckPasswordReqDto.builder()
+                .password("Test1234!")
+                .build();
+
+        //when
+        ResultActions result = mockMvc.perform(get(checkPasswordUrl, "1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(checkPasswordReqDto)));
+
+        // then
+        result.andExpectAll(
+                MockMvcResultMatchers.status().isOk(),
+                MockMvcResultMatchers.header().string(HttpHeaders.CONTENT_TYPE, "application/json"),
+                MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON),
+                MockMvcResultMatchers.jsonPath("$.message").value("인증되었습니다.")
+        ).andDo(print());
+    }
+
 //    @Test
 //    @DisplayName("고용주 - 회원정보 수정 성공")
 //    @WithMockUser
