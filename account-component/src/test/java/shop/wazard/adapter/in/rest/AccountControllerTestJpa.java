ef27792f3cd287024d2de449b68199a43a9b5bc9
min9yu98 <kimmingyu9811@gmail.com>
2023-05-02 01:25:38 +0900

    test:테스트코드 주석제거로 인한 import제거 복구

--- account-component/src/test/java/shop/wazard/adapter/in/rest/AccountControllerTestJpa.java
+++ account-component/src/test/java/shop/wazard/adapter/in/rest/AccountControllerTestJpa.java
@@ -6,9 +6,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import shop.wazard.WazardApplication;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 
 @SpringBootTest(classes = WazardApplication.class)
