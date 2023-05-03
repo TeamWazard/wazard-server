d78b0300c42d426d46a02eb91b895acd1ec4b165
simhani1 <simhani1@gmail.com>
2023-05-03 13:18:28 +0900

    feat: 로고 이미지 엔티티 추가

---
+++ entity-component/src/main/java/shop/wazard/entity/company/LogoImageJpa.java
@@ -0,0 +1,31 @@
+package shop.wazard.entity.company;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "LogoImage")
+public class LogoImageJpa {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "imageId")
+    private Long id;
+
+    private String imageName;
+
+    private String imageUrl;
+
+    @Builder
+    public LogoImageJpa(String imageName, String imageUrl) {
+        this.imageName = imageName;
+        this.imageUrl = imageUrl;
+    }
+
+}
