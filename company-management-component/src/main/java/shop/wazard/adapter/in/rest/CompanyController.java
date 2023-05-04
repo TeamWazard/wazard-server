c6d39c065c16a6276a6d11d8f0413255572375e0
wlsdbsrb <diejeadong@naver.com>
2023-05-04 22:28:15 +0900

    feat: 업장 등록 Controller 코드 작성

--- company-management-component/src/main/java/shop/wazard/adapter/in/rest/CompanyController.java
+++ company-management-component/src/main/java/shop/wazard/adapter/in/rest/CompanyController.java
@@ -1,9 +1,15 @@
 package shop.wazard.adapter.in.rest;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import shop.wazard.application.port.in.CompanyService;
+import shop.wazard.dto.RegisterCompanyReqDto;
+import shop.wazard.dto.RegisterCompanyResDto;
+import shop.wazard.util.aop.Certification;
+
+import javax.validation.Valid;
+import java.nio.file.AccessDeniedException;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,5 +17,12 @@ import shop.wazard.application.port.in.CompanyService;
 class CompanyController {
 
     private final CompanyService companyService;
+
+    @Certification
+    @PostMapping
+    public ResponseEntity<RegisterCompanyResDto> registerCompany(@PathVariable Long accountId, @Valid @RequestBody RegisterCompanyReqDto registerCompanyReqDto) throws AccessDeniedException, IllegalAccessException {
+        RegisterCompanyResDto registerCompanyResDto = companyService.registerCompany(registerCompanyReqDto);
+        return ResponseEntity.ok(registerCompanyResDto);
+    }
 
 }
