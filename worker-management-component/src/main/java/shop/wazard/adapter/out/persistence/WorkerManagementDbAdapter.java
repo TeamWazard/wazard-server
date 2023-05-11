9ee346e1aefba38b5a6a0bbadfae63c3ddf0c7dd
min9yu98 <kimmingyu9811@gmail.com>
2023-05-11 20:02:38 +0900

    feat: repository 추가 및 request dto 필요 어노테이션 추가

--- worker-management-component/src/main/java/shop/wazard/adapter/out/persistence/WorkerManagementDbAdapter.java
+++ worker-management-component/src/main/java/shop/wazard/adapter/out/persistence/WorkerManagementDbAdapter.java
@@ -2,6 +2,9 @@ package shop.wazard.adapter.out.persistence;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import shop.wazard.adapter.out.persistence.repository.AccountForWorkerManagementRepository;
+import shop.wazard.adapter.out.persistence.repository.RosterForWorkerManagementRepository;
+import shop.wazard.adapter.out.persistence.repository.WaitingListForWorkerManagementRepository;
 import shop.wazard.application.port.out.WorkerManagementPort;
 
 @Repository
@@ -9,6 +12,9 @@ import shop.wazard.application.port.out.WorkerManagementPort;
 class WorkerManagementDbAdapter implements WorkerManagementPort {
 
     private final WorkerManagementMapper workerForManagementMapper;
+    private final AccountForWorkerManagementRepository accountForWorkerManagementRepository;
+    private final RosterForWorkerManagementRepository rosterForWorkerManagementRepository;
+    private final WaitingListForWorkerManagementRepository waitingListForWorkerManagementRepository;
 
     @Override
     public void updateWorkerWaitingStatus(Long accountId, Long companyId) {
