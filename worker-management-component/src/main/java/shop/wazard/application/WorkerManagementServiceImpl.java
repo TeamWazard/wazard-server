d827d62f44f69d3bb53df9eaec972924db1b115b
simhani1 <simhani1@gmail.com>
2023-05-11 23:12:26 +0900

    fix: 응답값 수정

--- worker-management-component/src/main/java/shop/wazard/application/WorkerManagementServiceImpl.java
+++ worker-management-component/src/main/java/shop/wazard/application/WorkerManagementServiceImpl.java
@@ -32,7 +32,7 @@ class WorkerManagementServiceImpl implements WorkerManagementService {
         waitingInfo.changeWaitingStatus();
         rosterForWorkerManagementPort.joinWorker(RosterForWorkerManagement.createRosterForWorkerManagement(waitingInfo));
         return PermitWorkerToJoinResDto.builder()
-                .message("근무자 목록에 등록되었습니다.")
+                .message("업장 근무자 명단에 추가되었습니다.")
                 .build();
     }
 
