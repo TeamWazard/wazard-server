27d634227d8fac66f4dd414ff5c733d3420c9954
Jin Youn Gyu <88827805+wlsdbsrb@users.noreply.github.com>
2023-05-13 03:03:49 +0900

    feat: 근무자 결석 처리 service테스트코드 작성 및 구현

--- attendance-component/src/main/java/shop/wazard/application/AttendanceServiceImpl.java
+++ attendance-component/src/main/java/shop/wazard/application/AttendanceServiceImpl.java
@@ -3,7 +3,10 @@ package shop.wazard.application;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import shop.wazard.application.domain.AbsentForAttendance;
+import shop.wazard.application.domain.AccountForAttendance;
 import shop.wazard.application.port.in.AttendanceService;
+import shop.wazard.application.port.out.AbsentForAttendancePort;
 import shop.wazard.application.port.out.AccountForAttendancePort;
 import shop.wazard.application.port.out.CommuteRecordForAttendancePort;
 import shop.wazard.dto.MarkingAbsentReqDto;
@@ -16,10 +19,16 @@ class AttendanceServiceImpl implements AttendanceService {
 
     private final AccountForAttendancePort accountForAttendancePort;
     private final CommuteRecordForAttendancePort commuteRecordForAttendancePort;
+    private final AbsentForAttendancePort absentForAttendancePort;
 
     @Override
     public MarkingAbsentResDto markingAbsent(MarkingAbsentReqDto markingAbsentReqDto) {
-        return null;
+        AccountForAttendance accountForAttendance = accountForAttendancePort.findAccountByEmail(markingAbsentReqDto.getEmail());
+        accountForAttendance.checkIsEmployer();
+        absentForAttendancePort.markingAbsent(AbsentForAttendance.createAbsentForAttendance(markingAbsentReqDto));
+        return MarkingAbsentResDto.builder()
+                .message("결석 처리가 되었습니다.")
+                .build();
     }
 
 }
---
+++ attendance-component/src/main/java/shop/wazard/application/domain/AbsentForAttendance.java
@@ -0,0 +1,21 @@
+package shop.wazard.application.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import shop.wazard.dto.MarkingAbsentReqDto;
+
+@Getter
+@Builder
+public class AbsentForAttendance {
+
+    private Long accountId;
+    private Long companyId;
+
+    public static AbsentForAttendance createAbsentForAttendance(MarkingAbsentReqDto markingAbsentReqDto) {
+        return AbsentForAttendance.builder()
+                .accountId(markingAbsentReqDto.getAccountId())
+                .companyId(markingAbsentReqDto.getCompanyId())
+                .build();
+    }
+
+}
