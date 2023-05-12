89c0cbbbe6c3330024465f4124f69628449531c7
simhani1 <simhani1@gmail.com>
2023-05-12 20:20:38 +0900

    test: 업장 근무자 리스트 조회 repository 단위 테스트

--- worker-management-component/src/main/java/shop/wazard/adapter/out/persistence/CompanyJpaForWorkerManagementRepository.java
+++ worker-management-component/src/main/java/shop/wazard/adapter/out/persistence/CompanyJpaForWorkerManagementRepository.java
@@ -3,5 +3,5 @@ package shop.wazard.adapter.out.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shop.wazard.entity.company.CompanyJpa;
 
-interface CompanyForWorkerManagementRepository extends JpaRepository<CompanyJpa, Long> {
+interface CompanyJpaForWorkerManagementRepository extends JpaRepository<CompanyJpa, Long> {
 }
