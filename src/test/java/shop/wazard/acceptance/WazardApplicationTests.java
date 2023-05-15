f78b6e9bb369ea622c6ebd3d57785e4f517b1106
simhani1 <simhani1@gmail.com>
2023-05-16 05:14:10 +0900

    fix: 버스 수정

--- src/test/java/shop/wazard/acceptance/WazardApplicationTests.java
+++ src/test/java/shop/wazard/acceptance/WazardApplicationTests.java
@@ -1,8 +1,9 @@
-package shop.wazard;
+package shop.wazard.acceptance;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import shop.wazard.WazardApplication;
 
 @SpringBootTest
 @ContextConfiguration(classes = WazardApplication.class)
