0c6ca5800dbb77c0e22e6857e151b97f6fb4fd9e
simhani1 <simhani1@gmail.com>
2023-05-24 20:57:14 +0900

    feat: repository 구현

--- worker-management-component/src/main/java/shop/wazard/adapter/out/persistence/WaitingListJpaForWorkerManagementRepository.java
+++ worker-management-component/src/main/java/shop/wazard/adapter/out/persistence/WaitingListJpaForWorkerManagementRepository.java
@@ -1,7 +1,15 @@
 package shop.wazard.adapter.out.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import shop.wazard.entity.company.WaitingListJpa;
 
+import java.util.List;
+
 interface WaitingListJpaForWorkerManagementRepository extends JpaRepository<WaitingListJpa, Long> {
+
+    @Query("select w from WaitingListJpa w where w.companyJpa.id = :companyId and w.waitingStatusJpa <> 'JOINED' and w.baseStatusJpa = 'ACTIVE'")
+    List<WaitingListJpa> findWaitingWorkers(@Param("companyId") Long companyId);
+
 }
