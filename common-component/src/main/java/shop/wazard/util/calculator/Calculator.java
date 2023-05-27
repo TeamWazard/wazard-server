7838fc4b2c3b3ec0461f05399297a270f6abe7ec
min9yu98 <kimmingyu9811@gmail.com>
2023-05-27 21:54:00 +0900

    fix: 지각 점수 & 결석 점수 계산 로직 분리

--- common-component/src/main/java/shop/wazard/util/calculator/Calculator.java
+++ common-component/src/main/java/shop/wazard/util/calculator/Calculator.java
@@ -3,10 +3,18 @@ package shop.wazard.util.calculator;
 public class Calculator {
 
     public static double calculateAttitudeScore(int tardyCount, int absentCount, int totalWorkDay) {
-        double result = Math.round((10 - (10 * (tardyCount + absentCount * 5) / (double)totalWorkDay)) * 10) / 10.0;
+        double result = Math.round((10 - (10 * (calculateTardyScore(tardyCount) + calculateAbsentScore(absentCount)) / (double)totalWorkDay)) * 10) / 10.0;
         if (result < 0)
             result = 0;
         return result;
+    }
+
+    private static double calculateTardyScore(int tardyCount) {
+        return tardyCount;
+    }
+
+    private static double calculateAbsentScore(int absentCount) {
+        return absentCount * 5;
     }
 
 }
