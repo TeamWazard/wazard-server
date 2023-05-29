018d00a9fdee83aea09c009d2fcc512cb9dc6cde
wlsdbsrb <diejeadong@naver.com>
2023-05-29 18:51:23 +0900

    feat: 본인대타기록 조회 controller 구현

--- worker-component/src/main/java/shop/wazard/adapter/in/rest/WorkerController.java
+++ worker-component/src/main/java/shop/wazard/adapter/in/rest/WorkerController.java
@@ -4,11 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import shop.wazard.application.port.in.WorkerService;
+import shop.wazard.dto.GetMyReplaceReqDto;
+import shop.wazard.dto.GetMyReplaceResDto;
 import shop.wazard.dto.RegisterReplaceReqDto;
 import shop.wazard.dto.RegisterReplaceResDto;
 import shop.wazard.util.aop.Certification;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,6 +25,13 @@ class WorkerController {
     public ResponseEntity<RegisterReplaceResDto> registerReplace(@PathVariable Long accountId, @Valid @RequestBody RegisterReplaceReqDto registerReplaceReqDto) {
         RegisterReplaceResDto registerReplaceResDto = workerService.registerReplace(registerReplaceReqDto);
         return ResponseEntity.ok(registerReplaceResDto);
+    }
+
+    @Certification
+    @GetMapping("/replace/{accountId{")
+    public ResponseEntity<List<GetMyReplaceResDto>> getMyReplace(@PathVariable Long accountId, @Valid @RequestBody GetMyReplaceReqDto getMyReplaceReqDto) {
+        List<GetMyReplaceResDto> getMyReplaceResDtoList = workerService.getMyReplace(getMyReplaceReqDto);
+        return ResponseEntity.ok(getMyReplaceResDtoList);
     }
 
 }
