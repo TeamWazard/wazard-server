0a5a5fa70cfc316213b2e2f780b533ecacd5232a
min9yu98 <kimmingyu9811@gmail.com>
2023-06-02 00:10:09 +0900

    feat: 근무자 나의 태도 점수 조회 controller 개발 코드 구현

--- mypage-component/src/main/java/shop/wazard/adapter/in/rest/MyPageController.java
+++ mypage-component/src/main/java/shop/wazard/adapter/in/rest/MyPageController.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import shop.wazard.application.port.in.MyPageService;
-import shop.wazard.dto.GetMyPastWorkRecordReqDto;
-import shop.wazard.dto.GetMyPastWorkRecordResDto;
-import shop.wazard.dto.GetPastWorkplaceReqDto;
-import shop.wazard.dto.GetPastWorkplaceResDto;
+import shop.wazard.dto.*;
 import shop.wazard.util.aop.Certification;
 
 import javax.validation.Valid;
@@ -32,6 +29,13 @@ class MyPageController {
     public ResponseEntity<GetMyPastWorkRecordResDto> getMyPastWorkRecord(@PathVariable Long accountId, @Valid @RequestBody GetMyPastWorkRecordReqDto getMyPastWorkRecordReqDto) {
         GetMyPastWorkRecordResDto getMyPastWorkRecordResDto = myPageService.getMyPastWorkingRecord(getMyPastWorkRecordReqDto);
         return ResponseEntity.ok(getMyPastWorkRecordResDto);
+    }
+
+    @Certification
+    @GetMapping("/my-attitude-score/{accountId}")
+    public ResponseEntity<GetMyAttitudeScoreResDto> getMyAttitudeScore(@PathVariable Long accountId, @Valid @RequestBody GetMyAttitudeScoreReqDto getMyAttitudeScoreReqDto) {
+        GetMyAttitudeScoreResDto getMyAttitudeScoreResDto = myPageService.getMyAttitudeScore(getMyAttitudeScoreReqDto);
+        return ResponseEntity.ok(getMyAttitudeScoreResDto);
     }
 
 }
