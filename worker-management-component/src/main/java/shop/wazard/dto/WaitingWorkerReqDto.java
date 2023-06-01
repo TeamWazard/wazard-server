2d637d3caf891fbbcfb6a3882b3bfe2d33d26e23
Jonghan Sim <simhani1@gmail.com>
2023-06-01 23:58:00 +0900

    fix: companyId queray parameter로 변경

--- worker-management-component/src/main/java/shop/wazard/dto/WaitingWorkerReqDto.java
+++ worker-management-component/src/main/java/shop/wazard/dto/WaitingWorkerReqDto.java
@@ -4,16 +4,12 @@ import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Positive;
 
 @Getter
 @AllArgsConstructor
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class WaitingWorkerReqDto {
-
-    @Positive
-    private Long companyId;
 
     @Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "이메일 형식이 올바르지 않습니다.")
     @NotBlank(message = "이메일은 필수 입력 값입니다.")
