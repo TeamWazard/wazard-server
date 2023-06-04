4a4621ffd5c53a6f3107ded83f66334453b37dc9
Jonghan Sim <simhani1@gmail.com>
2023-06-05 01:43:08 +0900

    feat: 근무자 초대 메일 전송 service 구현
    * feat: 근무자 초대 메일 전송 service 구현
    
    * style:  spotless 적용
    
    * fix: service 인터페이스 수정
    
    * style: spotless 적용
    
    * fix: service 메서드 명 수정
    
    * style: spotless 적용

---
+++ smtp-component/src/main/java/shop/wazard/dto/InviteWorkerReqDto.java
@@ -0,0 +1,19 @@
+package shop.wazard.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class InviteWorkerReqDto {
+
+    @Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "이메일 형식이 올바르지 않습니다.")
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    private String email;
+
+    @NotBlank(message = "업장 명은 필수 입력 값입니다.")
+    private String companyName;
+}
