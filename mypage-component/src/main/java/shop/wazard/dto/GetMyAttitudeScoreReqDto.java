1709ccb599cb6c26f8eb023861adc61b38a22bdd
MinGyu <kimmingyu9811@gmail.com>
2023-06-05 01:55:45 +0900

    fix: 근무자 내 평균 태도 점수 조회 request dto 불필요 필드 삭제

--- mypage-component/src/main/java/shop/wazard/dto/GetMyAttitudeScoreReqDto.java
+++ mypage-component/src/main/java/shop/wazard/dto/GetMyAttitudeScoreReqDto.java
@@ -2,7 +2,6 @@ package shop.wazard.dto;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Positive;
 import lombok.*;
 
 @Getter
@@ -14,6 +13,4 @@ public class GetMyAttitudeScoreReqDto {
     @Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "이메일 형식이 올바르지 않습니다.")
     @NotBlank(message = "이메일은 필수 입력 값입니다.")
     private String email;
-
-    @Positive private Long companyId;
 }
