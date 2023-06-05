befe766d8fc52eb753a5d82e7cdaa13a1f892736
min9yu98 <kimmingyu9811@gmail.com>
2023-06-05 11:42:05 +0900

    feat: 계약 정보 및 관계 Entity 개발 초안

---
+++ entity-component/src/main/java/shop/wazard/entity/contract/ContractJpa.java
@@ -0,0 +1,27 @@
+package shop.wazard.entity.contract;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import shop.wazard.entity.account.AccountJpa;
+import shop.wazard.entity.common.BaseEntity;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "Contract")
+public class ContractJpa extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "contractId")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "accountId")
+    private AccountJpa accountJpa;
+
+    private boolean policyAgreeCheck;
+}
---
+++ entity-component/src/main/java/shop/wazard/entity/contract/ContractPaperJpa.java
@@ -0,0 +1,34 @@
+package shop.wazard.entity.contract;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import shop.wazard.entity.common.BaseEntity;
+import shop.wazard.entity.company.CompanyJpa;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "ContractPaper")
+public class ContractPaperJpa extends BaseEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "contractPaperId")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "companyId")
+    private CompanyJpa companyJpa;
+
+    private String contractPeriod;
+
+    private String workPlaceAddress;
+
+    private String workDayOfWeek;
+
+    private String workTime;
+
+    private int workPayment;
+}
